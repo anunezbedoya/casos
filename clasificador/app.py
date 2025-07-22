a5a56eaffca87246c781e422766e7fe8c0e13742
angie <mayli@unicauca.edu.co>
2025-07-22 13:03:32 -0500

    validar requiremntes

--- clasificador/app.py
+++ clasificador/app.py
@@ -4,5 +4,5 @@ import os
 app = Flask(__name__)
 app.register_blueprint(clasificador_bp, url_prefix='/clasificar')
 
-port = int(os.environ.get('PORT', 8080))
-app.run(debug=True, host='0.0.0.0', port=port)
+#port = int(os.environ.get('PORT', 8080))
+#app.run(debug=True, host='0.0.0.0', port=port)
