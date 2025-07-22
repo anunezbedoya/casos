8cd14bdcaffb5c1e2e44c01f7d491ac99ceca51a
angie <mayli@unicauca.edu.co>
2025-07-22 11:54:09 -0500

    validar requiremntes

--- clasificador/app.py
+++ clasificador/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from controllers.clasificador_controller import clasificador_bp
 
+# Crear la aplicación Flask
 app = Flask(__name__)
-app.register_blueprint(clasificador_bp, url_prefix='/clasificar')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8082)
+# Registrar el blueprint para el clasificador con el prefijo '/clasificar'
+app.register_blueprint(clasificador_bp, url_prefix='/clasificar')
